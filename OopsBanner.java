45f45cf93be62e23c57d28fc1799984dc94cb6dc
arunima nigam <aru.nig2606@gmail.com>
2026-03-03 12:11:31 +0530

    added UC4

--- OopsBanner.java
+++ OopsBanner.java
@@ -12,7 +12,7 @@
     }
 }*/
 
-public class OopsBanner {
+/*public class OopsBanner {
     public static void main(String[] args) {
 
         System.out.println(String.join("", "      ***      ***      *****       ***** "));
@@ -23,5 +23,27 @@ public class OopsBanner {
         System.out.println(String.join("", "     ** **    ** **    **             **  "));
         System.out.println(String.join("", "      ***      ***     **        *****    "));
 
+    }
+}*/
+public class OopsBanner{
+
+    public static void main(String[] args) {
+
+    
+        String[] banner = new String[7];
+
+    
+        banner[0] = String.join("", "      ***      ***      *****       ***** ");
+        banner[1] = String.join("", "     ** **    ** **    **   **    **      ");
+        banner[2] = String.join("", "    **   **  **   **   **   **   **       ");
+        banner[3] = String.join("", "    **   **  **   **   *****      ***     ");
+        banner[4] = String.join("", "    **   **  **   **   **            **   ");
+        banner[5] = String.join("", "     ** **    ** **    **             **  ");
+        banner[6] = String.join("", "      ***      ***     **        *****    ");
+
+        
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
